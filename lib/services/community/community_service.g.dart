5ef2f50b54e5664b68f09c7bbdcb179f49f2f630
molly <millie-molly@millie.town>
2022-06-20 17:09:20 +0900

    feat: 커뮤니티 진행중

---
+++ lib/services/community/community_service.g.dart
@@ -0,0 +1,67 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'community_service.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+class _CommunityService implements CommunityService {
+  _CommunityService(this._dio, {this.baseUrl}) {
+    baseUrl ??= 'http://qa-amos.vm-united.com';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<CommunityModel> getCommunityList(token, churchID) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<CommunityModel>(Options(
+                method: 'GET',
+                headers: <String, dynamic>{r'Authorization': token},
+                extra: _extra)
+            .compose(_dio.options,
+                '/api/v1/seum/church/$churchID/community/my/sharinggroup',
+                queryParameters: queryParameters, data: _data)
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = CommunityModel.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<CommunityModel> getCommunityDetail(
+      token, churchID, communityID) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<CommunityModel>(Options(
+                method: 'GET',
+                headers: <String, dynamic>{r'Authorization': token},
+                extra: _extra)
+            .compose(_dio.options,
+                '/api/v1/seum/church/$churchID/community/$communityID',
+                queryParameters: queryParameters, data: _data)
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = CommunityModel.fromJson(_result.data!);
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
